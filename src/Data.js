49f1b6c52ac68e5ec391b85029cf34a4cba9560a
Invdigitales <97065304+Invdigitales@users.noreply.github.com>
2022-09-15 20:15:22 -0300

    nuevo proyecto

---
+++ src/Data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Colchon - Goma espuma",
+    price: 3000,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-1.jpg",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Respaldo eco-cuero",
+    price: 2000,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-2.jpg",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "Base",
+    price: 5500,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-3.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Almohada",
+    price: 2500,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-4.jpg",
+  },
+];
